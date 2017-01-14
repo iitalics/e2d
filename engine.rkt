bfb6266f6a73e08b840df9ce4bac20b8326b13ca
Milo Turner <iitalics@gmail.com>
2017-01-14 05:12:41 -1000

    e2d engine hooks

--- engine.rkt
+++ engine.rkt
@@ -3,54 +3,88 @@
          sgl/gl
          data/gvector
          e2d/math)
+(provide
+ e2d-engine?
+ make-e2d-engine
+ current-e2d-engine
+ with-new-e2d-engine
+
+ e2d-windowed
+ e2d-fullscreen
+ e2d-start
+ e2d-get-size
+
+ e2d-add-hook
+ )
+
 
 (struct e2d-engine
   ([frame #:mutable]
    [look #:mutable]
    tick-hooks
    draw-hooks
-   extra-gl-hooks))
+   post-gl-hooks))
 
 (define (make-e2d-engine)
   (e2d-engine #f
-               '(800 600 #f)
-               (gvector)
-               (gvector)
-               (gvector)))
-
-(define (e2d-engine-title ng) "e2d")
-
+              '(800 600 #f)
+              (gvector)
+              (gvector)
+              (gvector)))
 
 ;; (current-e2d-engine) : e2d-engine?
 (define current-e2d-engine (make-parameter #f))
 
-;; (e2d-windowed w h : integer?
+;; (with-new-e2d-engine [fn : (-> any)]) -> any
+(define (with-new-e2d-engine fn)
+  (parameterize ([current-e2d-engine (make-e2d-engine)])
+    (fn)))
+
+;; (e2d-windowed [w h : integer?]
 ;;               [resize? : boolean? = #f]
 ;;               [ng : e2d-engine? = (current-e2d-engine)])
 (define (e2d-windowed w h [resizable? #f]
                       [ng (current-e2d-engine)])
   (set-e2d-engine-look! ng
-                         (list w h resizable?)))
+                        (list w h resizable?)))
 
+;; (e2d-fullscreen [ng : e2d-engine? = (current-e2d-engien)])
 (define (e2d-fullscreen [ng (current-e2d-engine)])
   (set-e2d-engine-look! ng
-                         'fullscreen))
+                        'fullscreen))
 
+;; (e2d-get-size [ng : e2d-engine? = (current-e2d-engine)])
+;;   -> integer?
+;;      integer?
 (define (e2d-get-size ng)
-  (match (e2d-engine-frame ng)
-    [#f (values 0 0)]
+  (match (or (e2d-engine-frame ng)
+             (e2d-engine-look ng))
+    [(list w h w?)
+     (values w h)]
     [frame
-     (send frame get-client-size)]))
+     (send frame get-client-size)]
+    [_
+     (values 0 0)]))
 
+;; (e2d-start [ng : e2d-engine? = (current-e2d-engine)])
 (define (e2d-start [ng (current-e2d-engine)])
   (let ([f (new e2d-frame% [ng ng])])
     (send f show #t)
     (send f tick)))
 
-(define (with-new-e2d-engine fn)
-  (parameterize ([current-e2d-engine (make-e2d-engine)])
-    (fn)))
+;; (e2d-add-hook [s : (or/c 'tick
+;;                         'draw
+;;                         'post-gl)]
+;;              [fn : (-> any)]
+;;              [ng : e2d-engine? = (current-e2d-engine)])
+(define (e2d-add-hook s fn [ng (current-e2d-engine)])
+  (gvector-add! (case s
+                  [(tick) (e2d-engine-tick-hooks ng)]
+                  [(draw) (e2d-engine-draw-hooks ng)]
+                  [(post-gl) (e2d-engine-post-gl-hooks ng)])
+                fn))
 
+;; Subclass of GUI frame%, for initializing the e2d engine's window.
 (define e2d-frame%
   (class* frame% ()
     (inherit show fullscreen
@@ -61,14 +95,14 @@
     ; determine frame style from ng
     (match (e2d-engine-look ng)
       [(list w h resiz?)
-       (super-new [label (e2d-engine-title ng)]
+       (super-new [label "e2d"]
                   [width w]
                   [height h]
                   [style (if resiz?
                              '()
                              '(no-resize-border))])]
       ['fullscreen
-       (super-new [label (e2d-engine-title ng)]
+       (super-new [label "e2d"]
                   [style '(fullscreen-button)])
        (fullscreen #t)])
 
@@ -78,6 +112,9 @@
 
     (define/public (tick)
       (when (is-shown?)
+        (parameterize ([current-e2d-engine ng])
+          (for ([fn (in-gvector (e2d-engine-tick-hooks ng))])
+            (fn)))
         (send canv refresh)
         (queue-callback (lambda () (tick)) #f)))
 
@@ -95,19 +132,17 @@
                [min-height 300])
     (init-field ng)
 
+    ;;; Draw the screen
     (define/override (on-paint)
       (with-gl-context
         (lambda ()
-          (glClearColor 0.0 0.0 1.0 1.0)
-          (glClear GL_COLOR_BUFFER_BIT)
-          (glFlush)
-          (glColor4f 1.0 0.0 0.0 1.0)
-          (glBegin GL_LINES)
-          (glVertex3f 0 0 0)
-          (let-values ([(w h) (e2d-get-size ng)])
-            (glVertex3f w h 0))
-          (glEnd)
-          (swap-gl-buffers))))
+          (parameterize ([current-e2d-engine ng])
+            (for ([fn (in-gvector (e2d-engine-draw-hooks ng))])
+              (fn))
+            (glFlush)
+            (swap-gl-buffers)
+            (for ([fn (in-gvector (e2d-engine-post-gl-hooks ng))])
+              (fn))))))
 
     (define/public (sized)
       (let-values ([(w h) (e2d-get-size ng)])
@@ -126,8 +161,3 @@
                      GL_ONE_MINUS_SRC_ALPHA)))
 
     ))
-
-(define (go)
-  (with-new-e2d-engine
-    (lambda ()
-      (e2d-start))))
